391f34ecf7e77b557e94d93404b11c2da04021b1
lapl29 <perazalugo1@gmail.com>
2023-12-02 20:54:32 -0700

    Avance

---
+++ 01-Fundamentos/assets/js/ciclos/ejemplo.js
@@ -0,0 +1,116 @@
+
+// const personajes = ['Goku','Vegeta','Trunks'];
+// const [ , , p3 ] = personajes;
+// console.log( p3 );
+
+
+// const retornaArreglo = () =>{
+//     return ['ABC', 123];
+// }
+
+// const [ letras, numeros ] = retornaArreglo(); 
+// console.log(letras, numeros);
+
+
+// // Tarea
+// // 1. el primer valor del arr se llamará nombre
+// // 2. se llamará setNombre
+
+// const useState = ( valor,valor2 ) => {
+//     return [ valor,valor2, ()=>{ console.log('Hola Mundo') } ];
+// }
+
+// const [ nombre,nombre2, setNombre ] = useState( 'Goku', 'Krilin' );
+
+// console.log( nombre,nombre2 );
+// setNombre();
+
+
+
+// function total (camisa,iva){
+//     return (camisa * iva);
+// }
+
+// const resultado = total(500,1.21);
+// console.log(resultado);
+
+
+
+// const numeros = [10, 20, 30, 40, 50];
+// numeros.forEach(function(numero,index) {
+//   console.log(numero,index);
+// });
+
+
+// const personajes = ['Goku','Vegeta','Trunks'];
+// const [ , , p3 ] = personajes;
+// console.log( p3 );
+
+
+// const retornaArreglo = () =>{
+//     return ['ABC', 123];
+// }
+
+// const [ letras, numeros ] = retornaArreglo(); 
+// console.log(letras, numeros);
+
+
+// // Tarea
+// // 1. el primer valor del arr se llamará nombre
+// // 2. se llamará setNombre
+
+// const useState = ( valor,valor2 ) => {
+//     return [ valor,valor2, ()=>{ console.log('Hola Mundo') } ];
+// }
+
+// const [ nombre,nombre2, setNombre ] = useState( 'Goku', 'Krilin' );
+
+// console.log( nombre,nombre2 );
+// setNombre();
+
+
+
+// function total (camisa,iva){
+//     return (camisa * iva);
+// }
+
+// const resultado = total(500,1.21);
+// console.log(resultado);
+
+
+
+// const numeros = [10, 20, 30, 40, 50];
+// numeros.forEach(function(numero,index) {
+//   console.log(numero,index);
+// });
+
+const nombres = ['LUIS', 'JOSIAS', 'MANUEL', 'PABLO', 'Mamberroi'];
+
+let cambioFondo = document.querySelector('body');
+
+let cambioh1 = document.querySelector('h1');
+
+cambioFondo.style.backgroundColor='blue';
+cambioh1.style.fontSize='5rem';
+
+
+let cambioColor = document.querySelectorAll('.hola');
+
+cambioColor.forEach(element => {
+  element.style.color ='white';
+});
+
+
+const nuevoDiv = document.createElement('div');
+
+nuevoDiv.textContent = 'hola bb';
+const body = document.body;
+
+
+
+// nombres.sort();
+// console.log(nombres);
+// nombres.sort((a,b) => a.length - b.length);
+// console.log(nombres);
+
+
